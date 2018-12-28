1e747a145bd3d0ad2f74e1219f9ee69c36ecd0fd
Beata Czekaj <czekajbeata@onet.pl>
2018-12-28 19:45:53 +0100

    separated usersController and profilesService into two separate

---
+++ ExchangeService/Controllers/Api/ProfilesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExchangeService.Controllers.Logic;
+using ExchangeService.Shared.Resources;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeService.Controllers.Api
+{
+    [ApiController]
+    public class ProfilesController : ControllerBase
+    {
+        private readonly ProfilesService profilesService;
+
+        public ProfilesController(ProfilesService profilesService)
+        {
+            this.profilesService = profilesService;
+        }
+
+        [HttpPost("api/users/profile")]
+        public IActionResult AddUserProfile([FromBody] UserView user)
+        {
+            string innerUserID = "1"; //only valid thing to receive from session
+            var result = profilesService.AddUserProfile(user, innerUserID);
+            return result ? (IActionResult)Ok() : BadRequest();
+        }
+
+        [HttpGet("api/users/{id?}")]
+        public UserView GetUserProfile(int id)
+        {
+            return profilesService.GetUserProfile(id);
+        }
+
+        [HttpGet("api/users/myprofile")]
+        public UserView GetMyUserProfile()
+        {
+            string myInnerId = "1"; //TODO dodać wyciąganie z sesji
+            var normalizedId = profilesService.ToNormalizedId(myInnerId);
+            return profilesService.GetUserProfile(normalizedId);
+        }
+    }
+}
---
+++ ExchangeService/Controllers/Api/UserDataController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExchangeService.Controllers.Logic;
+using ExchangeService.Shared.Resources;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeService.Controllers.Api
+{
+    [ApiController]
+    public class UserDataController : ControllerBase
+    {
+        private readonly UserDataService userDataService;
+        private readonly ProfilesService profilesService;
+
+        public UserDataController(UserDataService userDataService, ProfilesService profilesService)
+        {
+            this.userDataService = userDataService;
+            this.profilesService = profilesService;
+        }
+
+        [HttpPost("api/users/comments")]
+        public IActionResult AddComment([FromBody] CommentDto comment)
+        {
+            int currentUserId = 1; //TODO dodać wyciąganie z sesji
+            comment.LeavingUserId = currentUserId;
+            int receivingUserId = 2;
+
+            var result = userDataService.AddComment(comment, receivingUserId);
+            return result ? (IActionResult)Ok() : BadRequest();
+        }
+
+        [HttpPost("api/users/exchanges")]
+        public IActionResult AddExchange([FromBody] ExchangeDto exchange)
+        {
+            string myInnerId = "1"; //TODO dodać wyciąganie z sesji
+            var normalizedId = profilesService.ToNormalizedId(myInnerId);
+            var result = userDataService.AddExchange(exchange, normalizedId);
+            return result ? (IActionResult)Ok() : BadRequest();
+        }
+
+        [HttpGet("api/users/comments/{id?}")]
+        public IEnumerable<CommentDto> GetComments(int id)
+        {
+            return userDataService.GetComments(id);
+        }
+
+        [HttpGet("api/users/mymatches")]
+        public IEnumerable<MatchView> GetMatches()
+        {
+            string myInnerId = "1"; //TODO dodać wyciąganie z sesji
+            var normalizedId = profilesService.ToNormalizedId(myInnerId);
+            return userDataService.GetMatches(normalizedId);
+        }
+
+        [HttpGet("api/users/myexchanges")]
+        public IEnumerable<ExchangeDto> GetMyExchanges()
+        {
+            string myInnerId = "1"; //TODO dodać wyciąganie z sesji
+            var normalizedId = profilesService.ToNormalizedId(myInnerId);
+            return userDataService.GetUserExchanges(normalizedId);
+        }
+
+        [HttpGet("api/users/exchanges/{id?}")]
+        public IEnumerable<ExchangeDto> GetExchanges(int id)
+        {
+            return userDataService.GetUserExchanges(id);
+        }
+    }
+}
