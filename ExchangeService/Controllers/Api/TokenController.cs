933d01267a7e6720559ab814a56e0241860c0dd4
Beata Czekaj <czekajbeata@onet.pl>
2018-12-30 18:34:58 +0100

    part three of tutorial - working adding users to db

--- ExchangeService/Controllers/Api/TokenController.cs
+++ ExchangeService/Controllers/Api/TokenController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using ExchangeService.Controllers.Logic;
 using ExchangeService.Core;
 using ExchangeService.Shared.Resources;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExchangeService.Controllers.Api
@@ -12,19 +14,59 @@ namespace ExchangeService.Controllers.Api
     public class TokenController : ControllerBase
     {
         private readonly IJwtTokenService _tokenService;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public TokenController(IJwtTokenService tokenService)
+        public TokenController(IJwtTokenService tokenService, UserManager<IdentityUser> userManager)
         {
             _tokenService = tokenService;
+            _userManager = userManager;
         }
-
-        [HttpPost("api/token")]
-        public IActionResult GenerateToken([FromBody] TokenViewModel tokenvm)
+        
+        [Route("api/token")]
+        public string GenerateToken(string email)
         {
-            var token = _tokenService.BuildToken(tokenvm.Email);
-            return Ok(new { token });
+            var token = _tokenService.BuildToken(email);
+            return token;
         }
 
+        [HttpPost("api/token/registration")]
+        public async Task<IActionResult> Registration([FromBody] TokenViewModel tokenvm)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
 
+            var result = await _userManager.CreateAsync(new IdentityUser()
+            {
+                UserName = tokenvm.Email,
+                Email = tokenvm.Email
+            }, tokenvm.Password);
+
+            if(!result.Succeeded)
+            {
+               return StatusCode(500);
+            }
+            return Ok();
+        }
+
+        [HttpPost("api/token/login")]
+        public async Task<IActionResult> Login([FromBody] TokenViewModel tokenvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var user = await _userManager.FindByEmailAsync(tokenvm.Email);
+            var correctUser = await _userManager.CheckPasswordAsync(user, tokenvm.Password);
+
+            if(!correctUser)
+            {
+                return BadRequest("Username or password is incorrect");
+            }
+
+            return Ok(new { token = GenerateToken(tokenvm.Email) });
+        }
     }
 }
