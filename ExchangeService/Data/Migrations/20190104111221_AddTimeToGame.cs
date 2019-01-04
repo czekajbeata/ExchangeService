278ee0b2e92a12d42664d14173dd6d2245bb0f63
Beata Czekaj <czekajbeata@onet.pl>
2019-01-04 12:14:43 +0100

    added gametime clientside + migration

---
+++ ExchangeService/Data/Migrations/20190104111221_AddTimeToGame.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExchangeService.Data.Migrations
+{
+    public partial class AddTimeToGame : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GameTimeInMin",
+                table: "Games",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GameTimeInMin",
+                table: "Games");
+        }
+    }
+}
