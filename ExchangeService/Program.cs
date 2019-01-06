553af7a79732ab1a94a13d51937fce2303194708
Beata Czekaj <czekajbeata@onet.pl>
2019-01-06 20:22:23 +0100

    fixed clearing search box after search

--- ExchangeService/Program.cs
+++ ExchangeService/Program.cs
@@ -14,7 +14,7 @@ namespace ExchangeService
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).UseUrls("http://localhost:5000/").Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
